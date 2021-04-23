604d42e695a9fefe6a27e8745db55a6d42fa60bc
Sabboshachi Sarkar <55670542+Sabboshachi-Sarkar@users.noreply.github.com>
2021-04-24 02:46:25 +0600

    practice code update

---
+++ JavaPractice_from_Book/javaPractice01/src/com/sabboshachi/Main.java
@@ -0,0 +1,12 @@
+package com.sabboshachi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number = 100;
+	System.out.println("This is the original number " + number + "\n");
+
+	number = number * 2;
+	System.out.println("And here the number is multiplied by 2 so the result is " + number);
+    }
+}
